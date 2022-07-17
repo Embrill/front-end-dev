8c53c48881ff8c2d4cfcf29e954ac26d0acb0003
Vadim Kiryanov <vadim.kiryanov@mail.ru>
2022-07-17 21:17:40 +0300

    add js finale 1.0

--- js/app.js
+++ js/app.js
@@ -4,22 +4,33 @@
   setTimeout(() => {
     if (e) {
       let e = new Event("windowScroll");
-      window.addEventListener("scroll", function (n) {
+      window.addEventListener("scroll", function (o) {
         document.dispatchEvent(e);
       });
     }
-  }, 0),
+  }, 0);
+  const o = document.querySelector(".cookies"),
+    t =
+      (document.querySelector(".cookies-show"),
+      document.querySelector(".cookies-close"),
+      document.querySelector(".cookies__btn"));
+  setTimeout(() => {
+    o.classList.add("cookies-show");
+  }, 1e3),
+    (t.onclick = function () {
+      o.classList.add("cookies-close"), o.classList.remove("cookies-show");
+    }),
     (window.FLS = !0),
     (function (e) {
-      let n = new Image();
-      (n.onload = n.onerror =
+      let o = new Image();
+      (o.onload = o.onerror =
         function () {
-          e(2 == n.height);
+          e(2 == o.height);
         }),
-        (n.src =
+        (o.src =
           "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA");
     })(function (e) {
-      let n = !0 === e ? "webp" : "no-webp";
-      document.documentElement.classList.add(n);
+      let o = !0 === e ? "webp" : "no-webp";
+      document.documentElement.classList.add(o);
     });
 })();
