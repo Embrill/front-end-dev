8165046ba1adbe48c89c3730358799760494c93c
Vadim Kiryanov <vadim.kiryanov@mail.ru>
2022-07-17 21:17:40 +0300

    add js finale 1.0

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,19 @@
-// Импорт функционала ==============================================================================================================================================================================================================================================================================================================================
-// import { isMobile } from "./functions.js";
-// import { formsModules } from "./forms/forms.js";
+// ==========================================================
+// COOKIE
+const cookie = document.querySelector('.cookies');
+const cookieShow = document.querySelector('.cookies-show');
+const cookieClose = document.querySelector('.cookies-close');
+const cookieBtn = document.querySelector('.cookies__btn');
 
+// Появление куки через 1 сек
+setTimeout(() => {
+    cookie.classList.add('cookies-show')
+}, 1000);
+
+// Закрытие куки
+function closeCookie() {
+    cookie.classList.add('cookies-close');
+    cookie.classList.remove('cookies-show');
+}
+// Вызов ф-и закрытия куки
+cookieBtn.onclick = closeCookie;
